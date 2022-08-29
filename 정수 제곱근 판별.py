ec430a2b85157a220cc4fd155e850f5ef2e96e2f
yangsej <yangsej@gmail.com>
2022-08-29 23:03:12 +0900

    Create 정수 제곱근 판별.py

---
+++ 정수 제곱근 판별.py
@@ -0,0 +1,13 @@
+def solution(n):
+    answer = -1
+    
+    for i in range(1, n+1):
+        if i * i > n:
+            break
+        
+        elif i * i == n:
+            answer = (i+1) * (i+1)
+            break
+            
+    
+    return answer
